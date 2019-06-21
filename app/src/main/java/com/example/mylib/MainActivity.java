b9b14adb97ae2a2cbdbf2a4548be987f3d7b9a84
956263312@qq.com <956263312@qq.com>
2019-06-21 10:20:18 +0800

    更新状态栏

--- app/src/main/java/com/example/mylib/MainActivity.java
+++ app/src/main/java/com/example/mylib/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mylibrary.common.ImageLoaderManager;
+import com.example.mylibrary.utils.StatusBarUtil;
 import com.kongzue.dialog.v2.BottomMenu;
 
 public class MainActivity extends AppCompatActivity {
